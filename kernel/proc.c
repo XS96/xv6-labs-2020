729daffb33ade9454e6c03623dcae864969c15b7
sxuan <sxuan19965@gmail.com>
2024-06-25 13:54:18 +0000

    lab2-trace

--- kernel/proc.c
+++ kernel/proc.c
@@ -150,6 +150,7 @@ freeproc(struct proc *p)
   p->killed = 0;
   p->xstate = 0;
   p->state = UNUSED;
+  p->trace_mask = 0;
 }
 
 // Create a user page table for a given process,
@@ -276,6 +277,9 @@ fork(void)
   np->sz = p->sz;
 
   np->parent = p;
+  
+  //将trace_mask拷贝到子进程
+  np->trace_mask = p->trace_mask;
 
   // copy saved user registers.
   *(np->trapframe) = *(p->trapframe);
